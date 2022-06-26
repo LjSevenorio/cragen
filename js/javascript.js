7e843d3bd075fcd6081ddf0c2a205edf2dfdf863
cocopotato <57702745+cocopotato@users.noreply.github.com>
2022-06-26 09:17:09 +0800

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -24,6 +24,20 @@ $(document).ready(function () {
     });
   }
 
+  $(".accordion-tabs button").click(function () {
+    if ($('.tabItem1').hasClass('.accordion-active')) {
+      $('.tab2').hide();
+      $('.tab3').hide();
+    } else if ($('.tabItem2').hasClass('.accordion-active')) {
+      $('.tab1').hide();
+      $('.tab3').hide();
+    } else if ($('.tabItem3').hasClass('.accordion-active')) {
+      $('.tab1').hide();
+      $('.tab2').hide();
+    }
+    $('.tabs-slick').slick('refresh');
+  });
+
   // Scroll Up Nav
 
   if ($(window).width() > 768) {
@@ -55,13 +69,13 @@ $(document).ready(function () {
   });
 
 
-  // $(document).ready(function () {
-  //   $(window).on('resize', function () {
-  //     if ($(window).width() < 767) {
-  //       location.reload();
-  //     } else {}
-  //   });
-  // });
+  $(document).ready(function () {
+    $(window).on('resize', function () {
+      if ($(window).width() < 767) {
+        location.reload();
+      } else {}
+    });
+  });
 
   $("a").on("click", function (e) {
     if (this.hash !== "") {
@@ -102,8 +116,7 @@ $(document).ready(function () {
     focusOnSelect: true,
     autoplay: true,
     autoplaySpeed: 2000,
-    responsive: [
-      {
+    responsive: [{
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
@@ -126,15 +139,64 @@ $(document).ready(function () {
           slidesToScroll: 1
         }
       }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
     ]
   });
+
+  $('.banner-slick').slick({});
+
+  $('.tabs-slick').slick({});
 
   AOS.init({
     once: true
   });
+});
+
+$(document).ready(function () {
+
+
+
+  const labels = document.querySelectorAll(".accordion-item__label");
+  const tabs = document.querySelectorAll(".accordion-tab");
+
+  function toggleShow() {
+    const target = this;
+    const item = target.classList.contains("accordion-tab") ?
+      target :
+      target.parentElement;
+    const group = item.dataset.actabGroup;
+    const id = item.dataset.actabId;
+
+    tabs.forEach(function (tab) {
+      if (tab.dataset.actabGroup === group) {
+        if (tab.dataset.actabId === id) {
+          tab.classList.add("accordion-active");
+        } else {
+          tab.classList.remove("accordion-active");
+        }
+      }
+    });
+
+    labels.forEach(function (label) {
+      const tabItem = label.parentElement;
+
+      if (tabItem.dataset.actabGroup === group) {
+        if (tabItem.dataset.actabId === id) {
+          tabItem.classList.add("accordion-active");
+        } else {
+          tabItem.classList.remove("accordion-active");
+        }
+      }
+    });
+  }
+
+  labels.forEach(function (label) {
+    label.addEventListener("click", toggleShow);
+  });
+
+  tabs.forEach(function (tab) {
+    tab.addEventListener("click", toggleShow);
+  });
+
 });
 
 function setupFBframe(frame) {
@@ -198,168 +260,3 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 /* end Document Ready                       				   		
 ############################################ */
-
-// Slider(all Slides in a container)
-const slider = document.querySelector(".bannerslider")
-// All trails 
-const trail = document.querySelector(".trail").querySelectorAll("div")
-
-// Transform value
-let value = 0
-// trail index number
-let trailValue = 0
-// interval (Duration)
-let interval = 500000
-
-// Function to slide forward
-const slide = (condition) => {
-  // CLear interval
-  clearInterval(start)
-  // update value and trailValue
-  condition === "increase" ? initiateINC() : initiateDEC()
-  // move slide
-  move(value, trailValue)
-  // Restart Animation
-  animate()
-  // start interal for slides back 
-  start = setInterval(() => slide("increase"), interval);
-}
-
-// function for increase(forward, next) configuration
-const initiateINC = () => {
-  // Remove active from all trails
-  trail.forEach(cur => cur.classList.remove("active"))
-  // increase transform value
-  value === 75 ? value = 0 : value += 25
-  // update trailValue based on value
-  trailUpdate()
-}
-
-// function for decrease(backward, previous) configuration
-const initiateDEC = () => {
-  // Remove active from all trails
-  trail.forEach(cur => cur.classList.remove("active"))
-  // decrease transform value
-  value === 0 ? value = 75 : value -= 25
-  // update trailValue based on value
-  trailUpdate()
-}
-
-// function to transform slide 
-const move = (S, T) => {
-  // transform slider
-  slider.style.transform = `translateX(-${S}%)`
-  //add active class to the current trail
-  trail[T].classList.add("active")
-}
-
-const tl = gsap.timeline({
-  defaults: {
-    duration: 0.6,
-    ease: "power2.inOut"
-  }
-})
-tl.from(".bg", {
-    x: "-0%",
-    opacity: 0
-  })
-  .from(".bannercont p", {
-    opacity: 0
-  }, "-=0.3")
-  .from(".bannercont h1", {
-    opacity: 0,
-    y: "30px"
-  }, "-=0.3")
-
-// function to restart animation
-const animate = () => tl.restart()
-
-// function to update trailValue based on slide value
-const trailUpdate = () => {
-  if (value === 0) {
-    trailValue = 0
-  } else if (value === 25) {
-    trailValue = 1
-  } else if (value === 50) {
-    trailValue = 2
-  } else if (value === 75) {
-    trailValue = 3
-  }
-}
-
-// Start interval for slides
-let start = setInterval(() => slide("increase"), interval)
-
-// Next  and  Previous button function (SVG icon with different classes)
-document.querySelectorAll("svg").forEach(cur => {
-  // Assign function based on the class Name("next" and "prev")
-  cur.addEventListener("click", () => cur.classList.contains("next") ? slide("increase") : slide("decrease"))
-})
-
-// function to slide when trail is clicked
-const clickCheck = (e) => {
-  // CLear interval
-  clearInterval(start)
-  // remove active class from all trails
-  trail.forEach(cur => cur.classList.remove("active"))
-  // Get selected trail
-  const check = e.target
-  // add active class
-  check.classList.add("active")
-
-  // Update slide value based on the selected trail
-  if (check.classList.contains("box1")) {
-    value = 0
-  } else if (check.classList.contains("box2")) {
-    value = 25
-  } else if (check.classList.contains("box3")) {
-    value = 50
-  } else if (check.classList.contains("box4")) {
-    value = 75
-  }
-  // update trail based on value
-  trailUpdate()
-  // transfrom slide
-  move(value, trailValue)
-  // start animation
-  animate()
-  // start interval
-  start = setInterval(() => slide("increase"), interval)
-}
-
-// Add function to all trails
-trail.forEach(cur => cur.addEventListener("click", (ev) => clickCheck(ev)))
-
-// Mobile touch Slide Section
-const touchSlide = (() => {
-  let start, move, change, sliderWidth
-
-  // Do this on initial touch on screen
-  slider.addEventListener("touchstart", (e) => {
-    // get the touche position of X on the screen
-    start = e.touches[0].clientX
-    // (each slide with) the width of the slider container divided by the number of slides
-    sliderWidth = slider.clientWidth / trail.length
-  })
-
-  // Do this on touchDrag on screen
-  slider.addEventListener("touchmove", (e) => {
-    // prevent default function
-    e.preventDefault()
-    // get the touche position of X on the screen when dragging stops
-    move = e.touches[0].clientX
-    // Subtract initial position from end position and save to change variabla
-    change = start - move
-  })
-
-  const mobile = (e) => {
-    // if change is greater than a quarter of sliderWidth, next else Do NOTHING
-    change > (sliderWidth / 3) ? slide("increase") : null;
-    // if change * -1 is greater than a quarter of sliderWidth, prev else Do NOTHING
-    (change * -1) > (sliderWidth / 3) ? slide("decrease"): null;
-    // reset all variable to 0
-    [start, move, change, sliderWidth] = [0, 0, 0, 0]
-  }
-  // call mobile on touch end
-  slider.addEventListener("touchend", mobile)
-})()
