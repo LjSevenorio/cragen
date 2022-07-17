c9be58d03c42dafb1ecb0caa7b4b0b19635dfc12
LjSevenorio <57702745+LjSevenorio@users.noreply.github.com>
2022-07-17 12:34:12 +0800

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -178,6 +178,21 @@ $('.tabs-slick').slick({
   }]
 });
 
+$('.gallery-slick').slick({
+  prevArrow: false,
+  nextArrow: false,
+  responsive: [{
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 3000
+    }
+  }]
+});
+
 $('.menu-slick').slick({
   infinite: true,
   autoplay: true,
